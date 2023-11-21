7cbbc3fde14dec8c218ea72454e1b19ef4d738e0
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2023-11-21 15:12:11 -0300

    feat: add home page

---
+++ lib/presentation/pages/home_page/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:ebook_app/presentation/controllers/favorite_controller/favorite_controller.dart';
+import 'package:ebook_app/presentation/controllers/home_controller/home_controller.dart';
+import 'package:ebook_app/presentation/pages/shared/bookshelf_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '../shared/app_scaffold.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final homeController = Modular.get<HomeController>();
+  final favoriteController = Modular.get<FavoriteController>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    favoriteController.getAllFavorites();
+    homeController.fetchBooks();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScaffold(
+      title: 'Estante Virtual',
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ValueListenableBuilder(
+              valueListenable: homeController.books,
+              builder: (context, book, _) {
+                if (book.isEmpty) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return Expanded(
+                  child: BookshelfWidget(books: book),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
