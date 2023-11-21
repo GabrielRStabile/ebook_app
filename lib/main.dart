44f76f57ebdb37e5d690102d786b14c692320460
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2023-11-21 15:13:17 -0300

    feat: add HiveFavoriteBookRepository and ModularApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
+import 'package:ebook_app/infra/repositories/hive_favorite_book_repository.dart';
 import 'package:ebook_app/presentation/app_theme.dart';
+import 'package:ebook_app/presentation/routes/app_module.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Hive.initFlutter();
+
   runApp(const EBookApp());
 }
 
-class EBookApp extends StatelessWidget {
+class EBookApp extends StatefulWidget {
   const EBookApp({super.key});
 
   @override
+  State<EBookApp> createState() => _EBookAppState();
+}
+
+class _EBookAppState extends State<EBookApp> {
+  @override
+  void initState() {
+    super.initState();
+    HiveFavoriteBookRepository.registerAdapter();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'eBook',
-      theme: const AppTheme().toThemeData(),
-      debugShowCheckedModeBanner: false,
+    Modular.setInitialRoute('/home/');
+
+    return ModularApp(
+      module: AppModule(),
+      child: MaterialApp.router(
+        title: 'eBook',
+        theme: const AppTheme().toThemeData(),
+        routerConfig: Modular.routerConfig,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
